2725972011f4f62a11e66bff156f0fabd15c04bd
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2023-09-15 09:11:47 +0300

    bublik

---
+++ Rule/Pages/Admin.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Rule.Classes;
+using Rule.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rule
+{
+    /// <summary>
+    /// Логика взаимодействия для Admin.xaml
+    /// </summary>
+    public partial class Admin : Page
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void Vxod(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == "root" && password.Text == "12345")
+            {
+                Manager.MainFrame.Navigate(new adminik());
+            }
+        }
+
+        private void Checked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Collapsed;
+            password.Visibility = Visibility.Visible;
+            password.Text = passwordBox.Password;
+        }
+
+        private void Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Visible;
+            password.Visibility = Visibility.Collapsed;
+        }
+    }
+}
